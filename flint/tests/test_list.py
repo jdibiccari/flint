48859778ce987bcefc0f0a01487f12374355dd61
Janel diBiccari <jdibiccari@thuzio.com>
2016-07-05 22:14:08 -0400

    Test list command output

--- flint/tests/test_list.py
+++ flint/tests/test_list.py
@@ -17,27 +17,42 @@ class TestListInterface(unittest.TestCase):
         expected_output = get_message(LIST, 'not_found', {'project': 'A_Project_Has_No_Name'})
         self.assertEqual(result.output.strip('\n'), expected_output)
 
-    def test_list_project_pledges_target(self):
+    def test_list_project_pledges_target_not_reached(self):
         """It lists all pledges and informs that project target not reached"""
-        runner = CliRunner()
-        # Setup project with pledges
-        runner.invoke(flint, ['-t', 'project', '30_Rock_Returns', '300'])
+        expected_output = []
+        project = '30_Rock_Returns'
         pledges = [
             {'amount': 100, 'backer': 'Liz_Lemon', 'card': 348501267643300},
-            {'amount': 100, 'backer': 'Jenna_Maroney', 'card': 348501267643300}
+            {'amount': 100, 'backer': 'Jenna_Maroney', 'card': 377242569628624}
         ]
-        expected_output = []
+        runner = CliRunner()
+        runner.invoke(flint, ['-t', 'project', project, '300'])
+
         for pledge in pledges:
-            runner.invoke(flint, ['-t', 'back', pledge['backer'], '30_Rock_Returns', pledge['card'], str(project['amount'])])
+            runner.invoke(
+                flint, ['-t', 'back', pledge['backer'], project, str(pledge['card']), str(pledge['amount'])]
+            )
             expected_output.append(
                 get_message(LIST, 'pledge', pledge)
             )
-        expected_output.append(get_message([LIST], 'target_not_reached', {'project': '30_Rock_Returns', 'target_amount': 300}))
-        self.assertEqual(result.output.strip('\n'), expected_output)
 
-    def test_list_project_pledges_target(self):
+        expected_output.append(
+            get_message(LIST, 'target_not_reached', {'project': project, 'to_goal': 100})
+        )
+        result = runner.invoke(flint, ['-t', 'list', project])
+        self.assertEqual(result.output.strip('\n'), '\n'.join(expected_output))
+
+    def test_list_project_pledges_target_reached(self):
         """It lists all pledges and informs that project target reached"""
-        pass
+        project = 'Popcorn_Organizer'
+        pledge = {'amount': 10, 'backer': 'Kenneth', 'card': 5407598252092029}
+
+        runner = CliRunner()
+        runner.invoke(flint, ['-t', 'project', project, '10'])
+        runner.invoke(flint, ['-t', 'back', pledge['backer'], project, str(pledge['card']), str(pledge['amount'])])
+        result = runner.invoke(flint, ['-t', 'list', project])
+        expected_output = get_message(LIST, 'pledge', pledge) + '\n' + get_message(LIST, 'target_reached', {'project': project})
+        self.assertEqual(result.output.strip('\n'), expected_output)
 
     @classmethod
     def tearDownClass(cls):
